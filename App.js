df2085add936e275adac83ee370b861c237ca0a8
Yves Seurynck <yves.seurynck@vives.be>
2019-10-23 15:06:31 +0200

    remove justify content

--- App.js
+++ App.js
@@ -14,7 +14,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'stretch',
-    justifyContent: 'center'
+    alignItems: 'stretch'
   }
 });
